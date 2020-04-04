fab6520e22f0c719f16742a177a6fff5ba383cd1
kstawinski <stawinskidev@gmail.com>
2020-04-05 00:11:08 +0200

    add animation

--- src/views/Summary.vue
+++ src/views/Summary.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="summary">
-    <div class="summary_container">
-      <div class="summary_result">👌 <span class="summary_score">{{ getLastScore() }}</span></div>
-      <p class="summary_text">Maybe you'll try one more time?</p>
-      <button class="summary_button" @click="redirectToGame">Try again</button>
+  <transition name="bounce">
+    <div class="summary">
+      <div class="summary_container">
+        <div class="summary_result">👌 <span class="summary_score">{{ getLastScore() }}</span></div>
+        <p class="summary_text">Maybe you'll try one more time?</p>
+        <button class="summary_button" @click="redirectToGame">Try again</button>
+      </div>
     </div>
-  </div>
+  </transition>
 </template>
 
 <script>
@@ -63,5 +65,22 @@ export default {
           box-shadow: 0 0 10px rgba(237, 78, 60, 0.6);
         }
     }
+}
+.bounce-enter-active {
+  animation: bounce-in .5s;
+}
+.bounce-leave-active {
+  animation: bounce-in .5s reverse;
+}
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
 }
 </style>
